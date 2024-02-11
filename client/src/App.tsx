b2f32ddd119a1dfc088e27e8c5baa3504c28e49a
B E N <93186925+BenIzak@users.noreply.github.com>
2024-02-11 14:32:27 +0100

    add router navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
-import AuthPage from "./pages/Auth";
+import { lazy } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "@/components/Navbar";
+
+const AuthPage = lazy(() => import("@/pages/Auth"));
+const DashboardPage = lazy(() => import("@/pages/Dashboard"));
 
 export default function App() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
-      <AuthPage />
-    </div>
+    <Router>
+      <div className="h-screen w-screen flex flex-col items-center justify-start ">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/auth/*" element={<AuthPage />} />
+          <Route path="/dashboard/*" element={<DashboardPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
