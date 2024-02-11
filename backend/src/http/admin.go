a3f6b966dea8a2e945dd64e69b56652703fb3ae9
DewDonut <n_alary@hetic.eu>
2024-02-11 12:05:00 +0100

    implemented admin methods

---
+++ backend/src/http/admin.go
@@ -0,0 +1,66 @@
+package http
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/Watsuk/go-food/src/user"
+	"github.com/go-chi/chi"
+)
+
+func AdminEditEndpoint(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		userIDString := chi.URLParam(r, "userID")
+		var userModel AdminUserBody
+		err := json.NewDecoder(r.Body).Decode(&userModel)
+
+		if userIDString == "" || err != nil{
+			http.Error(w, "Invalid user ID or data", http.StatusBadRequest)
+			return
+		}
+
+		userID, err := strconv.ParseInt(userIDString, 10, 64)
+		if err != nil {
+			http.Error(w, "Invalid user ID", http.StatusBadRequest)
+			return
+		}
+
+		err = user.EditUser(db, userID, userModel.Username, userModel.Email, userModel.Role)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusNoContent)
+	}
+}
+
+func AdminDeleteEndpoint(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		userIDString := chi.URLParam(r, "userID")
+		if userIDString == "" {
+			http.Error(w, "Invalid user ID", http.StatusBadRequest)
+			return
+		}
+
+		userID, err := strconv.ParseInt(userIDString,10,64)
+		if err != nil {
+			http.Error(w, "Invalid user ID", http.StatusBadRequest)
+			return
+		}
+
+		err = user.DeleteUser(db, userID)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusNoContent)
+	}
+}
+
+type AdminUserBody struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Role     int    `json:"role"`
+}
